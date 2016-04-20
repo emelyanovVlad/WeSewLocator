fc1ed8eea54aa2ae404c8a694f3ba50512fb6154
Vladyslav Yemelianov <Vladyslav_Yemelianov@epam.com>
2016-04-20 21:51:25 +0300

    branch service done

--- src/main/java/com/we/sew/locator/service/app/BranchService.java
+++ src/main/java/com/we/sew/locator/service/app/BranchService.java
@@ -3,7 +3,9 @@ package com.we.sew.locator.service.app;
 import com.we.sew.locator.bean.BranchBean;
 import com.we.sew.locator.db.entity.Branch;
 import com.we.sew.locator.db.entity.SystemUser;
+import com.we.sew.locator.db.repository.BranchRepository;
 import com.we.sew.locator.service.app.api.IBranchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +15,9 @@ import java.util.List;
  */
 @Service
 public class BranchService implements IBranchService {
+    @Autowired
+    private BranchRepository branchRepository;
+
     @Override
     public Branch getBy(String name) {
         return null;
