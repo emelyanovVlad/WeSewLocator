5944cd654a5113e3c64caeed1c4aaa19516a9ff0
Vladyslav Yemelianov <Vladyslav_Yemelianov@epam.com>
2016-04-21 17:44:08 +0300

    branch question service done

--- src/main/java/com/we/sew/locator/service/app/BranchQuestionService.java
+++ src/main/java/com/we/sew/locator/service/app/BranchQuestionService.java
@@ -2,7 +2,10 @@ package com.we.sew.locator.service.app;
 
 import com.we.sew.locator.db.entity.BranchQuestion;
 import com.we.sew.locator.db.entity.SystemUser;
+import com.we.sew.locator.db.repository.BranchQuestionRepository;
+import com.we.sew.locator.service.app.api.AbstractService;
 import com.we.sew.locator.service.app.api.IBranchQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,29 +14,36 @@ import java.util.List;
  * @author Vladyslav_Yemelianov
  */
 @Service
-public class BranchQuestionService implements IBranchQuestionService {
+public class BranchQuestionService extends AbstractService implements IBranchQuestionService {
+    @Autowired
+    private BranchQuestionRepository branchQuestionRepository;
+
     @Override
     public void create(BranchQuestion el, SystemUser creator) {
-
+        creationUpdateInfoEntityFiller.fill(el, creator);
+        branchQuestionRepository.save(el);
     }
 
     @Override
-    public BranchQuestion get(Long aLong) {
-        return null;
+    public BranchQuestion get(Long id) {
+        return branchQuestionRepository.getOne(id);
     }
 
     @Override
     public List<BranchQuestion> getAll() {
-        return null;
+        return branchQuestionRepository.findAll();
     }
 
     @Override
     public void update(BranchQuestion el, SystemUser updater) {
-
+        editionUpdateInfoEntityFiller.fill(el, updater);
+        branchQuestionRepository.save(el);
     }
 
     @Override
-    public BranchQuestion delete(Long el) {
-        return null;
+    public BranchQuestion delete(Long id) {
+        BranchQuestion deletedBranchQuestion = get(id);
+        branchQuestionRepository.delete(id);
+        return deletedBranchQuestion;
     }
 }
