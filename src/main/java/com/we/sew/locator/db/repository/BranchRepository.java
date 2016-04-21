0d5ae7fd3c747cca419b8609d5e8aa1cca576c4d
Vladyslav Yemelianov <Vladyslav_Yemelianov@epam.com>
2016-04-21 12:46:50 +0300

    branch service done

--- src/main/java/com/we/sew/locator/db/repository/BranchRepository.java
+++ src/main/java/com/we/sew/locator/db/repository/BranchRepository.java
@@ -4,9 +4,14 @@ import com.we.sew.locator.db.entity.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Vladyslav_Yemelianov
  */
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Integer> {
+    Branch findOneByName(String name);
+
+    List<Branch> findByNameContaining(String name);
 }
--- src/main/java/com/we/sew/locator/service/app/BranchService.java
+++ src/main/java/com/we/sew/locator/service/app/BranchService.java
@@ -4,7 +4,9 @@ import com.we.sew.locator.bean.BranchBean;
 import com.we.sew.locator.db.entity.Branch;
 import com.we.sew.locator.db.entity.SystemUser;
 import com.we.sew.locator.db.repository.BranchRepository;
+import com.we.sew.locator.service.app.api.AbstractService;
 import com.we.sew.locator.service.app.api.IBranchService;
+import com.we.sew.locator.util.adapter.BranchAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,42 +16,49 @@ import java.util.List;
  * @author Vladyslav_Yemelianov
  */
 @Service
-public class BranchService implements IBranchService {
+public class BranchService extends AbstractService implements IBranchService {
     @Autowired
     private BranchRepository branchRepository;
+    @Autowired
+    private BranchAdapter branchAdapter;
 
     @Override
     public Branch getBy(String name) {
-        return null;
+        return branchRepository.findOneByName(name);
     }
 
     @Override
     public List<Branch> getAllBy(String name) {
-        return null;
+        return branchRepository.findByNameContaining(name);
     }
 
     @Override
     public void create(BranchBean el, SystemUser creator) {
-
+        Branch adaptedBranch = branchAdapter.adapt(el);
+        creationUpdateInfoEntityFiller.fill(adaptedBranch, creator);
+        branchRepository.save(adaptedBranch);
     }
 
     @Override
-    public Branch get(Integer integer) {
-        return null;
+    public Branch get(Integer id) {
+        return branchRepository.getOne(id);
     }
 
     @Override
     public List<Branch> getAll() {
-        return null;
+        return branchRepository.findAll();
     }
 
     @Override
     public void update(Branch el, SystemUser updater) {
-
+        editionUpdateInfoEntityFiller.fill(el, updater);
+        branchRepository.save(el);
     }
 
     @Override
-    public Branch delete(Integer el) {
-        return null;
+    public Branch delete(Integer id) {
+        Branch deletedBranch = get(id);
+        branchRepository.delete(id);
+        return deletedBranch;
     }
 }
