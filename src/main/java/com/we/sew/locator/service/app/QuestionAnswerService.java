c30a0af208464ba6a3b59f55327bd27de2eda98f
Vladyslav Yemelianov <Vladyslav_Yemelianov@epam.com>
2016-04-21 18:12:35 +0300

    question answer service done

--- src/main/java/com/we/sew/locator/service/app/QuestionAnswerService.java
+++ src/main/java/com/we/sew/locator/service/app/QuestionAnswerService.java
@@ -2,7 +2,10 @@ package com.we.sew.locator.service.app;
 
 import com.we.sew.locator.db.entity.QuestionAnswer;
 import com.we.sew.locator.db.entity.SystemUser;
+import com.we.sew.locator.db.repository.QuestionAnswerRepository;
+import com.we.sew.locator.service.app.api.AbstractService;
 import com.we.sew.locator.service.app.api.IQuestionAnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,29 +14,36 @@ import java.util.List;
  * @author Vladyslav_Yemelianov
  */
 @Service
-public class QuestionAnswerService implements IQuestionAnswerService {
+public class QuestionAnswerService extends AbstractService implements IQuestionAnswerService {
+    @Autowired
+    private QuestionAnswerRepository questionAnswerRepository;
+
     @Override
     public void create(QuestionAnswer el, SystemUser creator) {
-
+        creationUpdateInfoEntityFiller.fill(el, creator);
+        questionAnswerRepository.save(el);
     }
 
     @Override
-    public QuestionAnswer get(Long aLong) {
-        return null;
+    public QuestionAnswer get(Long id) {
+        return questionAnswerRepository.getOne(id);
     }
 
     @Override
     public List<QuestionAnswer> getAll() {
-        return null;
+        return questionAnswerRepository.findAll();
     }
 
     @Override
     public void update(QuestionAnswer el, SystemUser updater) {
+        editionUpdateInfoEntityFiller.fill(el, updater);
 
     }
 
     @Override
-    public QuestionAnswer delete(Long el) {
-        return null;
+    public QuestionAnswer delete(Long id) {
+        QuestionAnswer deletedQuestionAnswer = get(id);
+        questionAnswerRepository.delete(id);
+        return deletedQuestionAnswer;
     }
 }
