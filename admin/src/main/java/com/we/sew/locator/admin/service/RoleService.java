c45672fde273dcdbf6472156f2436195aa17fb4a
Vladyslav Yemelianov <Vladyslav_Yemelianov@epam.com>
2016-05-13 13:53:31 +0300

    role service updated

--- admin/src/main/java/com/we/sew/locator/admin/service/RoleService.java
+++ admin/src/main/java/com/we/sew/locator/admin/service/RoleService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Vladyslav_Yemelianov
@@ -45,7 +47,10 @@ public class RoleService extends AbstractService implements IRoleService {
 
     @Override
     public List<Role> getAll() {
-        return roleRepository.findAll();
+        return roleRepository.findAll().stream().map(role -> {
+            role.setName(role.getName().substring(ROLE.length()));
+            return role;
+        }).collect(Collectors.toList());
     }
 
     @Override
